62e0e9580e71b47b5736ae3e737a23c0572b2faa
Logipek <hugoguttr@gmail.com>
2025-03-01 21:28:00 +0100

    feat(pokemon): Add getters and setters for Pokemon class properties

--- src/main/java/javamon/modele/pokemon.java
+++ src/main/java/javamon/modele/pokemon.java
@@ -20,7 +20,7 @@ public class Pokemon {
   private List<Type> types;
   private List<Attaque> attaques;
   private ObjetTenu objetTenu;
-  private Status status;
+  private statut statut;
   // image du pokemon arriere plan et font pour javafx
   private String imageFont;
   private String imageBack;
@@ -33,7 +33,8 @@ public class Pokemon {
   private double facteurVitesse = 1.0;
 
   // constructeur avec les propriétés du pokemon
-  public Pokemon(String nom, int hpMax, int attaque, int defense, int attaqueSpeciale, int defenseSpeciale, int vitesse, List<Type> types) {
+  public Pokemon(String nom, int hpMax, int attaque, int defense, int attaqueSpeciale, int defenseSpeciale, int vitesse,
+      List<Type> types) {
     this.nom = nom;
     this.hpMax = hpMax;
     this.hp.set(hpMax);
@@ -44,5 +45,85 @@ public class Pokemon {
     this.vitesse = vitesse;
     this.types = new ArrayList<>(types);
     this.attaques = new ArrayList<>();
+  }
+
+  public String getNom() {
+    return nom;
+  }
+
+  public int getHp() {
+    return hp.get();
+  }
+
+  public IntegerProperty hpProperty() {
+    return hp;
+  }
+
+  public int getHpMax() {
+    return hpMax;
+  }
+
+  public int getAttaque() {
+    return (int) (attaque * facteurAttaque);
+  }
+
+  public int getDefense() {
+    return (int) (defense * facteurDefense);
+  }
+
+  public int getAttaqueSpeciale() {
+    return (int) (attaqueSpecial * facteurAttaqueSpecial);
+  }
+
+  public int getDefenseSpeciale() {
+    return (int) (defenseSpecial * facteurDefenseSpecial);
+  }
+
+  public int getVitesse() {
+    return (int) (vitesse * facteurVitesse);
+  }
+
+  public List<Type> getTypes() {
+    return new ArrayList<>(types);
+  }
+
+  public List<Attaque> getAttaques() {
+    return new ArrayList<>(attaques);
+  }
+
+  public ObjetTenu getObjetTenu() {
+    return objetTenu;
+  }
+
+  public void setObjetTenu(ObjetTenu objetTenu) {
+    this.objetTenu = objetTenu;
+  }
+
+  public Statut getStatut() {
+    return statut;
+  }
+
+  public void setStatut(Statut statut) {
+    this.statut = statut;
+    if (statut != null) {
+      statut.appliquerEffetDebut(this);
     }
+  }
+
+  public String getImageFront() {
+    return imageFront;
+  }
+
+  public void setImageFront(String imageFront) {
+    this.imageFront = imageFront;
+  }
+
+  public String getImageBack() {
+    return imageBack;
+  }
+
+  public void setImageBack(String imageBack) {
+    this.imageBack = imageBack;
+  }
+
 }
