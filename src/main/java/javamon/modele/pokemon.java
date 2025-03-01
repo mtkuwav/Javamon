fed8d98d6df28a93270096408e9b115e0c1e9c47
Logipek <hugoguttr@gmail.com>
2025-03-01 18:29:45 +0100

    feat(pokemon): Add damage and stat multiplier factors to Pokemon class

--- src/main/java/javamon/modele/pokemon.java
+++ src/main/java/javamon/modele/pokemon.java
@@ -24,4 +24,25 @@ public class pokemon {
   // image du pokemon arriere plan et font pour javafx
   private String imageFont;
   private String imageBack;
+
+  // facteur de dégats pour les attaques et heal 
+  private double facteurAttaque = 1.0;
+  private double facteurDefense = 1.0;
+  private double facteurAttaqueSpecial = 1.0;
+  private double facteurDefenseSpecial = 1.0;
+  private double facteurVitesse = 1.0;
+
+  // constructeur avec les propriétés du pokemon
+  public pokemon(String nom, int hpMax, int attaque, int defense, int attaqueSpecial, int defenseSpecial, int vitesse, List<Type> types, List<Attaque> attaques, ObjetTenu objetTenu, Status status, String imageFont, String imageBack) {
+    this.nom = nom;
+    this.hpMax = hpMax;
+    this.hp.set(hpMax);
+    this.attaque = attaque;
+    this.defense = defense;
+    this.attaqueSpecial = attaqueSpecial;
+    this.defenseSpecial = defenseSpecial;
+    this.vitesse = vitesse;
+    this.types = new ArrayList<>(types);
+    this.attaaques = new ArrayList<>();
+  }
 }
