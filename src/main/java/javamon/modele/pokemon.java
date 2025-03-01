c7974635bdb32baf68dfbb2083752c8376b65137
Logipek <hugoguttr@gmail.com>
2025-03-01 18:23:38 +0100

    feat(pokemon): Add basic Pokemon class structure with properties

--- src/main/java/javamon/modele/pokemon.java
+++ src/main/java/javamon/modele/pokemon.java
@@ -1,3 +1,27 @@
+package javamon.modele;
+
+import java.util.ArrayList;
+import java.util.Random;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class pokemon {
-  
+  // integer pour rafraichir la valeur de hp dans javafx
+  private IntegerProperty hp = new SimpleIntegerProperty();
+  // propriétés du pokemon
+  private String nom;
+  private int hpMax;
+  private int attaque;
+  private int defense;
+  private int attaqueSpecial;
+  private int defenseSpecial;
+  private int vitesse;
+  // liste des types pour option de séléction
+  private List<Type> types;
+  private List<Attaque> attaques;
+  private ObjetTenu objetTenu;
+  private Status status;
+  // image du pokemon arriere plan et font pour javafx
+  private String imageFont;
+  private String imageBack;
 }
